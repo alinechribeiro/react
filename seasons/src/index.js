c8a28057384f38b7141e300ae0bf2054198cd5e2
Aline <alinechribeiro@gmail.com>
2020-06-24 15:25:55 +0100

    lifecycle methods

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
-const App = () => {
+class App extends React.Component {
 
-    //call Geolocation API
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    );
-    return <div>Latitude: </div>;
-};
+    constructor(props) {
+        super(props);
+
+        // THIS IS THE ONLY TIME we do direct assignment
+        // to this.state
+        this.state = { lat: null, erorMessage: '' };
+    }
+    
+    state = { lat: null, errorMessage: '' };
+
+    componentDidMount() {
+        //call Geolocation API
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
+        );
+    }
+
+    componentDidUpdate() {
+        console.log('My component has updated.');
+    }
+    //React says we have to define render!!
+    render () {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>; 
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            // return <div>Latitute: {this.state.lat}</div>;
+            return <SeasonDisplay lat={this.state.lat} />
+        }
+
+        return <div>Loading!</div>;
+    }
+}
 
 ReactDOM.render(<App />,document.querySelector('#root'));
